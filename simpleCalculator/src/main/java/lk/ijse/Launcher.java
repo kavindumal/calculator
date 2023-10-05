ed930f281d1165ddf859248ee660cf6fe6d983d3
kavindumal <kavindu11250403@gmail.com>
2023-10-05 15:53:45 +0530

    Launcher

--- simpleCalculator/src/main/java/lk/ijse/Launcher.java
+++ simpleCalculator/src/main/java/lk/ijse/Launcher.java
@@ -1,7 +1,27 @@
 package lk.ijse;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Launcher extends Application {
     public static void main(String[] args) {
-        System.out.println("hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/calculatorForm.fxml")));
+
+        Scene scene = new Scene(rootNode);
+
+        stage.setTitle("Login page");
+        stage.centerOnScreen();
+        stage.setScene(scene);
+        stage.show();
     }
 }
